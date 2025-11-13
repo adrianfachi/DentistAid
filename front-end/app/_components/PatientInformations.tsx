3bd9520c14edabfdc63a8303b3359d6f1bfb8bd1
adrianfachi <adrianfachi5@gmail.com>
2025-11-13 15:27:20 -0300

    Alterando o Patient View

---
+++ front-end/app/_components/PatientInformations.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {
+  patient: patientType;
+}
+
+function PatientInformations({ patient }: Props) {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default PatientInformations
--- front-end/app/_components/PatientView.tsx
+++ front-end/app/_components/PatientView.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+'use client';
+
+import React, { useState } from 'react'
+import { FiEdit } from 'react-icons/fi';
 
 type Props = {
   patient: patientType | undefined
@@ -7,14 +10,24 @@ type Props = {
 function PatientView({ patient }: Props) {
 
   if (patient) {
+    const [editing, setEditing] = useState<boolean>(false)
     const colors = ["bg-ligth-green", "bg-dark-green", "bg-green"];
     const bgClass = colors[patient!.patientId % 3];
     const index = patient!.name.trim().lastIndexOf(" ")
     const initialNameLetter = patient!.name[0].toUpperCase() + (index != -1 && patient!.name[index + 1].toUpperCase());
 
     return (
-      <div className={`${bgClass} w-20 h-20 flex justify-center text-3xl items-center rounded-full text-white font-bold`}>
-        {initialNameLetter}
+      <div>
+        <div className='flex gap-2 items-center'>
+          <div className={`${bgClass} w-20 h-20 flex justify-center text-3xl items-center rounded-full text-white font-bold`}>
+            {initialNameLetter}
+          </div>
+          <h2>
+            {patient.name} | {patient.patientId.toString().padStart(4, "0")}
+          </h2>
+          <FiEdit className={`${!editing && "text-gray "} cursor-pointer`} onClick={() => { setEditing(!editing) }} />
+        </div>
+
       </div>
     )
   }
