1728c963855a456902847b8c0f1c2311a21b961b
lorenzoalig <lorenzoalig@gmail.com>
2025-11-23 23:42:42 -0300

    Fixed module imports for PostModule

--- backend/src/domain/patient-aggregate/modules/post.module.ts
+++ backend/src/domain/patient-aggregate/modules/post.module.ts
@@ -2,10 +2,12 @@ import { Module } from "@nestjs/common";
 import { PostController } from "src/application/controllers/post.controller";
 import { PostService } from "../services/post.service";
 import { PostMapper } from "../mappers/post.mapper";
+import { InfrastructureModule } from "src/infrastructure/modules/infrastructure.module";
 
 
 @Module({
     controllers: [PostController],
     providers: [PostService, PostMapper],
+    imports: [InfrastructureModule],
 })
 export class PostModule {}
