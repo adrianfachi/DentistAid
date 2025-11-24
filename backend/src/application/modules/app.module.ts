0c1ee115467f91276ad6883cef88be62c2af69e2
lorenzoalig <lorenzoalig@gmail.com>
2025-11-24 04:23:45 -0300

    Fixed Module imports in AppointmentModule

--- backend/src/application/modules/app.module.ts
+++ backend/src/application/modules/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AppointmentModule } from 'src/domain/calender-aggregate/modules/appointment.module';
 import { PatientModule } from 'src/domain/patient-aggregate/modules/patient.module';
 import { PostModule } from 'src/domain/patient-aggregate/modules/post.module';
 import { InfrastructureModule } from 'src/infrastructure/modules/infrastructure.module';
 
 
 @Module({
-  imports: [PatientModule, PostModule, InfrastructureModule],
+  imports: [PatientModule, PostModule, AppointmentModule, InfrastructureModule],
   controllers: [],
   providers: [],
 })
--- backend/src/domain/calender-aggregate/modules/appointment.module.ts
+++ backend/src/domain/calender-aggregate/modules/appointment.module.ts
@@ -2,11 +2,12 @@ import { Module } from "@nestjs/common";
 import { AppointmentController } from "src/application/controllers/appointment.controller";
 import { AppointmentService } from "../services/appointment.service";
 import { InfrastructureModule } from "src/infrastructure/modules/infrastructure.module";
+import { AppointmentMapper } from "../mappers/appointment.mapper";
 
 
 @Module({
     controllers: [AppointmentController],
-    providers: [AppointmentService],
+    providers: [AppointmentService, AppointmentMapper],
     imports: [InfrastructureModule]
 })
 export class AppointmentModule {}
